3dd848dc295e2b045899d7c0f25da36cdb241408
Praveen-p-k <rpraveenkumarr51@gmail.com>
2023-06-23 20:42:20 +0530

    First Commit

---
+++ DB-Management/CRUD.js
@@ -0,0 +1,13 @@
+const mongodb = require("mongodb");
+const Client = mongodb.MongoClient;
+
+const throwDataBase = async (_) => {
+  const connection = await Client.connect(
+    "mongodb+srv://praveenkumarr:Rch8WthDmxFj5JYF@cluster0.eps7eee.mongodb.net/?retryWrites=true&w=majority"
+  );
+  let database = connection.db("tsProject");
+
+  if (!database) console.log("Not Connected Database");
+  return database;
+};
+module.exports = { throwDataBase };
